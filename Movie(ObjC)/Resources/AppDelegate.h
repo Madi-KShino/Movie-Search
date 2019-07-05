e72971d4ed6f303777bba5738bbacecadfbae932
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-07-05 09:18:09 -0600

    Initial Commit

---
+++ Movie(ObjC)/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movie(ObjC)
+//
+//  Created by Madison Kaori Shino on 7/5/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Movie(ObjC)/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Movie(ObjC)
+//
+//  Created by Madison Kaori Shino on 7/5/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
